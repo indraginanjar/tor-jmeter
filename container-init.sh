2855ffebabf8016a8ef07be2fba11eac5ed2e890
Indra Ginanjar <indraginanjar@gmail.com>
2021-12-06 13:04:58 +0700

    proxy used by jmeter is could be changed by modifying enviroment variables TORJM_PROXY_HOST and TORJM_PROXY_PORT

--- container-init.sh
+++ container-init.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 tor --runasdaemon 1
 polipo proxyAddress=0.0.0.0 proxyPort=8123 socksParentProxy="127.0.0.1:9050" socksProxyType=socks5 diskCacheRoot="" logLevel=0x0 daemonise=true
-/usr/local/openjdk-8/bin/java -jar /home/jmeter/apache-jmeter-5.2/bin/ApacheJMeter.jar -n -H 127.0.0.1 -P 8123 $@
+
+if [ $TORJM_PROXY_HOST=="" ];
+	then TORJM_PROXY_HOST="127.0.0.1";
+fi
+
+if [ $TORJM_PROXY_PORT=="" ];
+	then TORJM_PROXY_PORT="8123";
+fi
+
+/usr/local/openjdk-8/bin/java -jar /home/jmeter/apache-jmeter-5.2/bin/ApacheJMeter.jar -n -H $TORJM_PROXY_HOST -P $TORJM_PROXY_PORT $@
